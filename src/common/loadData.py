3f8ef8d1806bf28bd7b5e86fc17954992e7a7882
anhtuan <anhtuan122536@gmail.com>
2024-10-13 08:04:41 +0700

    Sửa file load data lân 2

--- src/common/loadData.py
+++ src/common/loadData.py
@@ -11,35 +11,42 @@ map_stance = {'agree': 0, 'disagree': 1, 'discuss': 2}
 
 def load_all_data(file_in, type_class, features):
     jsonl_reader = JSONLineReader()
-    # Sử dụng file_in trực tiếp thay vì nối với os.getcwd()
     datas = jsonl_reader.read(file_in)
     df_in = pd.DataFrame(datas)
+
+    # In ra danh sách cột để kiểm tra
+    print("Columns in DataFrame:", df_in.columns.tolist())
+
     label = 'label'
 
-    # Chuyển đổi nhãn theo loại lớp
     if type_class == 'related':
         df_in[label] = df_in[label].replace(['agree', 'disagree', 'discuss'], 'related')
     elif type_class == 'stance':
         df_in = df_in[df_in[label] != 'unrelated']
         df_in = df_in.reset_index(drop=True)
 
-    # Chuyển đổi nhãn thành số
     df_in[label] = labelencoder.fit_transform(df_in[label])
 
     l = df_in[label].to_list()
     h = df_in['sentence1'].to_list()
-    p = df_in['sentence2'].to_list()  # Sửa 'sentences2' thành 'sentence2'
+
+    # Kiểm tra tên cột
+    if 'sentence2' in df_in.columns:
+        p = df_in['sentence2'].to_list()
+    else:
+        raise KeyError("'sentence2' column is missing in the DataFrame")
 
     f = []
     if features:
         df = pd.DataFrame(df_in[features[0]].to_list(), columns=['0'])
         for i in range(1, len(features)):
             df[str(i)] = df_in[features[i]].to_list()
-        f = df.values.tolist()  # Lấy giá trị từ DataFrame và chuyển đổi thành danh sách
+        f = df.values.tolist()
     else:
-        f = [0] * len(h)  # Tạo một danh sách bằng 0 nếu không có features
+        f = [0] * len(h)
 
     list_of_tuples = list(zip(h, p, l, f))
     return pd.DataFrame(list_of_tuples, columns=['text_a', 'text_b', 'labels', 'feature'])
+
 
 
