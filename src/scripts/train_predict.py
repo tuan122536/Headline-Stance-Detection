c5827b0142110e85067724627c593f198690a2b1
anhtuan <anhtuan122536@gmail.com>
2024-10-15 06:19:10 +0700

    sua file train lan 3

--- src/scripts/train_predict.py
+++ src/scripts/train_predict.py
@@ -1,13 +1,11 @@
-import argparse
+﻿import argparse
 from common.loadData import load_all_data
 from model.roberta_model import train_predict_model, predict
 
-
 feature_stance = ['polarityClaim_nltk_neg', 'polarityClaim_nltk_pos', 'polarityBody_nltk_neg', 'polarityBody_nltk_pos']
 feature_related = ['cosine_similarity', 'max_score_in_position', 'overlap', 'soft_cosine_similarity', 'bert_cs']
-featfeature_all = ['cosine_similarity', 'max_score_in_position', 'overlap', 'soft_cosine_similarity', 'bert_cs',
+feature_all = ['cosine_similarity', 'max_score_in_position', 'overlap', 'soft_cosine_similarity', 'bert_cs',
                'polarityClaim_nltk_neg', 'polarityClaim_nltk_pos', 'polarityBody_nltk_neg', 'polarityBody_nltk_pos']
-
 
 
 def main(parser):
@@ -20,6 +18,7 @@ def main(parser):
     test_set = args.test_set
     model_dir = args.model_dir
     feature = []
+    
     if not not_use_feature:
         if type_class == 'stance':
             feature = feature_stance
@@ -28,9 +27,16 @@ def main(parser):
         elif type_class == 'all':
             feature = feature_all
 
+    # Chỉnh sửa đường dẫn đến tập dữ liệu
+    training_set = training_set.replace("/data/", "/content/apex/Headline-Stance-Detection/")
+    test_set = test_set.replace("/data/", "/content/apex/Headline-Stance-Detection/")
+
     if model_dir == "":
         df_test = load_all_data(test_set, type_class, feature)
         df_train = load_all_data(training_set, type_class, feature)
+        
+        # In ra thông tin huấn luyện
+        print("Bắt đầu huấn luyện mô hình...")
         train_predict_model(df_train, df_test, True, use_cuda, len(feature))
     else:
         df_test = load_all_data(test_set, type_class, feature)
@@ -39,8 +45,8 @@ def main(parser):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    
     ## Required parameters
-
     parser.add_argument("--type_class", choices=['related', 'stance', 'all'],
                         default='related', help="This parameter is used to choose the type of "
                                                 "clasificator (related, stance, all).")
@@ -61,7 +67,7 @@ if __name__ == '__main__':
                         help="This parameter is the relatis_type_contradictionive dir of training set.")
 
     parser.add_argument("--test_set",
-                        default="/content/apex/Headline-Stance-Detection/FNC_summy_textRank_test_spacy_pipeline_polarity_v2.json",
+                        default="/data/FNC_summy_textRank_test_spacy_pipeline_polarity_v2.json",
                         type=str,
                         help="This parameter is the relative dir of test set.")
 
