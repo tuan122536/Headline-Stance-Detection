295ebcd35adab8e49761c4f542594fa9fb57df70
Tuan Le <112860468+tuan122536@users.noreply.github.com>
2024-10-22 10:49:53 +0700

    Update OutRobertaClassificationHead.py

--- src/model/OutRobertaClassificationHead.py
+++ src/model/OutRobertaClassificationHead.py
@@ -4,16 +4,14 @@ from torch.nn import CrossEntropyLoss, MSELoss
 from transformers import BertModel, BertConfig
 
 class OutRobertaClassificationHead(nn.Module):
-    """Head for sentence-level classification tasks."""
-
     def __init__(self, config):
         super().__init__()
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
-    def forward(self, features, externalFeature=None):
-        x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
+    def forward(self, features, externalFeature=None):  # Chấp nhận externalFeature
+        x = features[:, 0, :]  # Lấy token <s>
         x = self.dropout(x)
         x = self.dense(x)
         x = torch.tanh(x)
