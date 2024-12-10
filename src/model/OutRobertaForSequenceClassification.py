acb83f2c2d8fcdb2c53a938b03b15bbcecd07a48
Tuan Le <112860468+tuan122536@users.noreply.github.com>
2024-12-10 20:31:12 +0700

    Update OutRobertaForSequenceClassification.py

--- src/model/OutRobertaForSequenceClassification.py
+++ src/model/OutRobertaForSequenceClassification.py
@@ -2,9 +2,9 @@ from simpletransformers.classification.transformer_models.roberta_model import R
 import torch
 import torch.nn as nn
 from torch.nn import CrossEntropyLoss, MSELoss
-from transformers.modeling_roberta import (
+from transformers.models.bert.modeling_bert import BertPreTrainedModel
+from transformers.models.roberta.modeling_roberta import (
     ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST,
-    BertPreTrainedModel,
     RobertaClassificationHead,
     RobertaConfig,
     RobertaModel,
